8549a01fc188854ef47755627e45c9d45a07b66c
hardym3n <dpaslamuskas@gmail.com>
2021-07-04 02:28:31 +0600

    Add course

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AccountProfile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE),
+    name = models.CharField(max_length=63)
+    surname = models.CharField(max_length=63, null=True)
+    third_name = models.CharField(max_length=63, null=True)
+    age = models.IntegerField(null=True, blank=True)
+    status = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
